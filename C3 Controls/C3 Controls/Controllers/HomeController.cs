d931628131244d2f7f79ef684bcc77a1d1f81b27
Alex Mammay <amm7100@psu.edu>
2017-02-06 09:30:23 -0500

    push 1
    Signed-off-by: Alex Mammay <amm7100@psu.edu>

---
+++ C3 Controls/C3 Controls/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using C3_Controls.Models;
+using System.Web.Mvc;
+
+namespace C3_Controls.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Configuration()
+        {
+            return View();
+        }
+
+        public ActionResult ConfigureTowerLight()
+        {
+            return View(new TowerLight());
+        }
+
+        public ActionResult ConfigurePushToTest()
+        {
+            return View(new PushToTest());
+        }
+    }
+}

8b80ddf86f7a5dcf39571f1ba1056ce2931bfc32
Alex Mammay <amm7100@psu.edu>
2017-02-27 21:05:07 -0500

    Adding in _Clean.bat so we dont have to worry about obj and bin files

---
+++ TylerC3 Controls/C3 Controls/_Clean.bat
@@ -0,0 +1,12 @@
+:: Clean a Visual Studio project by removing the bin and obj directories.
+:: This script was created because Visual Studio's Clean command doesn't
+:: always remove the artifacts from the build.
+:: 2/27/2017 Alex Mammay
+
+ECHO "Clean the Visual Studio Solution by removing the bin and obj directories"
+
+:: Look for "bin", "obj", "Setup", and "BuildResults" directories and remove them
+for /d /r . %%d in (bin,obj,Setup,BuildResults) do @if exist "%%d" rd /s/q "%%d"
+
+del *.log /Q
+del *.suo 
--- TylerC3 Controls/C3 Controls/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="C3_Controls.MvcApplication" Language="C#" %>

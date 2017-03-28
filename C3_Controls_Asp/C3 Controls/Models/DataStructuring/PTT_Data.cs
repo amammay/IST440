db51a06ffd1ddb9cefd6840b5c6914d456ae8ea7
Alex Mammay <amm7100@psu.edu>
2017-03-27 22:36:25 -0400

    Refactored and resequenced the way the data is being deliverd to the ui

---
+++ C3_Controls_Asp/C3 Controls/Models/DataStructuring/PTT_Data.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace C3_Controls.Models.DataStructuring
+{
+    public class PTT_Data
+    {
+        public Dictionary<string, PTTItem[]> PttMap { get; set; }
+        public string CmSetting => WebConfigurationManager.AppSettings["CurrentDatebase"];
+
+
+        
+        public List<PricedItem> Voltages { get; set; }
+        public List<PricedItem> LampTypeColor { get; set; }
+        public List<PricedItem> ClampRing { get; set; }
+        public List<PricedItem> LensType { get; set; }
+        public List<PricedItem> LensColor { get; set; }
+        public PricedItem Options { get; set; }
+
+
+
+        public PTT_Data()
+        {
+            if (CmSetting != "internal")
+            {
+                CouchDbConnector myCouchDbConnector = new CouchDbConnector();
+
+                PttMap = new Dictionary<string, PTTItem[]>();
+
+                //Assign our dictionary to the one that was populated when the connection was made 
+                PttMap = myCouchDbConnector.PttMap;
+            }
+
+            Voltages = new List<PricedItem>();
+
+            //Cycle over our wtl map 
+            foreach (var valueSet in PttMap)
+            {
+                List<PTTItem> valueSetItems = new List<PTTItem>();
+
+                //TODO document
+                foreach (var valueSetItem in valueSet.Value)
+                {
+                    valueSetItems.Add(valueSetItem);
+                }
+
+                
+
+                switch (valueSet.Key)
+                {
+                    case "Full_Voltage":
+                        
+                        
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            Voltages.Add(singleItem);
+                        }
+                        break;
+
+                    case "Transformer (50/60 Hz)":
+                        //Voltages = new List<PricedItem>();
+
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            Voltages.Add(singleItem);
+                        }
+                        break;
+
+                    case "Resistor":
+                       // Voltages = new List<PricedItem>();
+
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            Voltages.Add(singleItem);
+                        }
+                        break;
+
+                    case "Lamp Type And Color":
+                        LampTypeColor = new List<PricedItem>();
+
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            LampTypeColor.Add(singleItem);
+                        }
+                        break;
+                    case "Clamp Ring":
+                        ClampRing = new List<PricedItem>();
+
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            ClampRing.Add(singleItem);
+                        }
+                        break;
+                    case "Lens Type":
+                        LensType = new List<PricedItem>();
+
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            LensType.Add(singleItem);
+                        }
+                        break;
+                    case "Lens Color":
+                        LensColor = new List<PricedItem>();
+
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            LensColor.Add(singleItem);
+                        }
+                        break;
+
+                        case "Options":
+                        foreach (var item in valueSetItems)
+                        {
+                            Options = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                           
+                        }
+                        break;
+
+
+
+
+                }
+
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ C3_Controls_Asp/C3 Controls/Models/DataStructuring/WTL_Data.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace C3_Controls.Models.DataStructuring
+{
+    public class WTL_Data
+    {
+        public Dictionary<string, WTLItem[]> WtlMap { get; set; }
+        //public Dictionary<string, PTTItem[]> PttMap { get; set; }
+        public string CmSetting => WebConfigurationManager.AppSettings["CurrentDatebase"];
+
+
+        public PricedItem OperatorType { get; set; }
+        public PricedItem ModuelDiameter { get; set; }
+        public List<PricedItem> Voltages { get; set; }
+        public List<PricedItem> Styles { get; set; }
+        public List<PricedItem> Positions { get; set; }
+        public List<PricedItem> SoundModule { get; set; }
+        public List<PricedItem> LightLens { get; set; }
+
+        public WTL_Data()
+        {
+            if (CmSetting != "internal")
+            {
+                CouchDbConnector myCouchDbConnector = new CouchDbConnector();
+
+                WtlMap = new Dictionary<string, WTLItem[]>();
+
+                //Assign our dictionary to the one that was populated when the connection was made 
+                WtlMap = myCouchDbConnector.WtlMap;
+            }
+
+            //Creats a list of all of our keys in the dictionary
+            List<string> WtlKeyMap = new List<string>();
+
+            //Iterate over items in our map and add them to key list
+            foreach (var key in WtlMap.Keys)
+            {
+
+                WtlKeyMap.Add(key);
+
+            }
+
+            //Cycle over our wtl map 
+            foreach (var valueSet in WtlMap)
+            {
+                List<WTLItem> valueSetItems = new List<WTLItem>();
+
+                //TODO document
+                foreach (var valueSetItem in valueSet.Value)
+                {
+                    valueSetItems.Add(valueSetItem);
+                }
+
+                switch (valueSet.Key)
+                {
+                    case "Operator Type":
+                        //TODO document
+                        foreach (var item in valueSetItems)
+                        {
+                            OperatorType = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Sku = item.sku,
+                                Desc = item.text
+                                
+                            };
+
+                        }
+                        break;
+
+                    case "Diameter":
+                        //TODO document
+                        foreach (var item in valueSetItems)
+                        {
+                            ModuelDiameter = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Sku = item.sku,
+                                Desc = item.text
+
+                            };
+
+                        }
+                        break;
+
+                    case "Base Material & Style":
+                        Styles = new List<PricedItem>();
+                        //TODO document
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            Styles.Add(singleItem);
+                        }
+                        break;
+
+                    case "Voltage":
+                        Voltages = new List<PricedItem>();
+                        //TODO document
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            Voltages.Add(singleItem);
+                        }
+                        break;
+
+                    case "Light Lens":
+                        LightLens = new List<PricedItem>();
+                        //TODO document
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            LightLens.Add(singleItem);
+                        }
+                        break;
+
+                    case "Sound Module":
+                        SoundModule = new List<PricedItem>();
+                        //TODO document
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            SoundModule.Add(singleItem);
+                        }
+                        break;
+
+                    case "Position":
+                        Positions = new List<PricedItem>();
+                        //TODO document
+                        foreach (var item in valueSetItems)
+                        {
+                            PricedItem singleItem = new PricedItem
+                            {
+                                Name = item.text,
+                                Price = item.price,
+                                Desc = item.text,
+                                Sku = item.sku
+                            };
+                            //Add her to the list
+                            Positions.Add(singleItem);
+                        }
+                        break;
+
+                }
+
+
+            }
+        }
+
+        
+       
+
+    }
+}

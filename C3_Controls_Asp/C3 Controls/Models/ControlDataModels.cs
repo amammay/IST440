6227fea10f66775f52db30307d910c434cc02c05
Alex Mammay <amm7100@psu.edu>
2017-03-29 00:29:23 -0400

    slight refactoring and optimzation of the DataStructuring , added in special cases for images for world tower lights

--- C3_Controls_Asp/C3 Controls/Models/ControlDataModels.cs
+++ C3_Controls_Asp/C3 Controls/Models/ControlDataModels.cs
@@ -1,56 +1,27 @@
 ﻿using System.Collections.Generic;
-using System.Web.Mvc;
-using System.Web.Configuration;
 using C3_Controls.Models.DataStructuring;
 
 namespace C3_Controls.Models
 {
-    /// <summary>
-    ///     This is a base product and contains all the
-    ///     information that is the same amongst all products.
-    /// </summary>
-    public abstract class Control
-    {
-        public static double DEFAULT_PRICE = 0.0;
-        public abstract PricedItem OperatorType { get; }
-        public abstract List<PricedItem> Voltages { get; }
-        public WTL_Data WtlData { get; set; }
-        public PTT_Data PttData { get; set; }
-
-        // Converts a string list into a select list for drop downs
-        public static IEnumerable<SelectListItem> Convert(List<string> items)
-        {
-            var listItems = new List<SelectListItem>();
-            foreach (var item in items)
-                listItems.Add(new SelectListItem {Text = item, Value = item});
-            return listItems;
-        }
-        
-
-    }
-
-
     /// <summary>
     ///     This is the Tower Light product. It contains information
     ///     pertaining to only the Tower Light.
     /// </summary>
     public class TowerLight : Control
     {
-
+        //Get instance of WtlData so we can set our data context
         public TowerLight()
         {
             WtlData = new WTL_Data();
         }
 
         public override PricedItem OperatorType => WtlData.OperatorType;
-
         public override List<PricedItem> Voltages => WtlData.Voltages;
-
         public PricedItem ModuleDiameter => WtlData.ModuelDiameter;
-
         public List<PricedItem> Styles => WtlData.Styles;
-
         public List<PricedItem> Positions => WtlData.Positions;
+        public List<PricedItem> SoundModule => WtlData.SoundModule;
+        public List<PricedItem> LightLens => WtlData.LightLens;
     }
 
 
@@ -60,24 +31,20 @@ namespace C3_Controls.Models
     /// </summary>
     public class PushToTest : Control
     {
-
+        //Get instance of PttData to set our data context
         public PushToTest()
         {
             PttData = new PTT_Data();
         }
 
-        public override PricedItem OperatorType => new PricedItem {Name = "Push to Test", Sku = "PTT", Price = DEFAULT_PRICE};
+        public override PricedItem OperatorType
+            => new PricedItem {Name = "Push to Test", Sku = "PTT", Price = DEFAULT_PRICE};
 
         public override List<PricedItem> Voltages => PttData.Voltages;
-
         public List<PricedItem> LampColors => PttData.LampTypeColor;
-
         public List<PricedItem> ClampRings => PttData.ClampRing;
-
         public List<PricedItem> LensTypes => PttData.LensType;
-
         public List<PricedItem> LensColors => PttData.LensColor;
-
         public PricedItem Options => PttData.Options;
     }
 }
--- C3_Controls_Asp/C3 Controls/Models/DataStructuring/WTL_Data.cs
+++ C3_Controls_Asp/C3 Controls/Models/DataStructuring/WTL_Data.cs
@@ -1,122 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Configuration;
 
 namespace C3_Controls.Models.DataStructuring
 {
     public class WTL_Data
     {
-        public Dictionary<string, WTLItem[]> WtlMap { get; set; }
-        //public Dictionary<string, PTTItem[]> PttMap { get; set; }
-        public string CmSetting => WebConfigurationManager.AppSettings["CurrentDatebase"];
-
-
-        public PricedItem OperatorType { get; set; }
-        public PricedItem ModuelDiameter { get; set; }
-        public List<PricedItem> Voltages { get; set; }
-        public List<PricedItem> Styles { get; set; }
-        public List<PricedItem> Positions { get; set; }
-        public List<PricedItem> SoundModule { get; set; }
-        public List<PricedItem> LightLens { get; set; }
-
+        /// <summary>
+        ///     This is were dictionary specific values get their proper inheritence of a priced item
+        /// </summary>
         public WTL_Data()
         {
+            //Quick check to see if we are local or using external couchdb 
             if (CmSetting != "internal")
             {
-                CouchDbConnector myCouchDbConnector = new CouchDbConnector();
+                //Instance of couch connector
+                var myCouchDbConnector = new CouchDbConnector();
 
+                //Creates our map
                 WtlMap = new Dictionary<string, WTLItem[]>();
 
                 //Assign our dictionary to the one that was populated when the connection was made 
                 WtlMap = myCouchDbConnector.WtlMap;
             }
 
-            //Creats a list of all of our keys in the dictionary
-            List<string> WtlKeyMap = new List<string>();
-
-            //Iterate over items in our map and add them to key list
-            foreach (var key in WtlMap.Keys)
-            {
-
-                WtlKeyMap.Add(key);
-
-            }
 
             //Cycle over our wtl map 
             foreach (var valueSet in WtlMap)
             {
-                List<WTLItem> valueSetItems = new List<WTLItem>();
+                var valueSetItems = new List<WTLItem>();
 
-                //TODO document
+                //Iterate over each value set, its going to be a wtl item 
                 foreach (var valueSetItem in valueSet.Value)
-                {
                     valueSetItems.Add(valueSetItem);
-                }
 
+                //Switch on the value set key
                 switch (valueSet.Key)
                 {
+                    //Handles operator types
                     case "Operator Type":
-                        //TODO document
                         foreach (var item in valueSetItems)
-                        {
                             OperatorType = new PricedItem
                             {
                                 Name = item.text,
                                 Price = item.price,
                                 Sku = item.sku,
                                 Desc = item.text
-                                
                             };
-
-                        }
                         break;
 
                     case "Diameter":
-                        //TODO document
                         foreach (var item in valueSetItems)
-                        {
                             ModuelDiameter = new PricedItem
                             {
                                 Name = item.text,
                                 Price = item.price,
                                 Sku = item.sku,
                                 Desc = item.text
-
                             };
-
-                        }
                         break;
 
                     case "Base Material & Style":
                         Styles = new List<PricedItem>();
-                        //TODO document
                         foreach (var item in valueSetItems)
                         {
-                            PricedItem singleItem = new PricedItem
+                            var singleItem = new PricedItem
                             {
                                 Name = item.text,
                                 Price = item.price,
                                 Desc = item.text,
                                 Sku = item.sku
                             };
-                            //Add her to the list
+
+                            //extra logic to assign an image to the item
+                            switch (singleItem.Sku)
+                            {
+                                case "P1":
+                                    singleItem.Img = "img_base_short.png";
+                                    break;
+                                case "P2":
+                                    singleItem.Img = "img_base_long.png";
+                                    break;
+                                case "P3":
+                                    singleItem.Img = "img_base_short.png";
+                                    break;
+                            }
+
+                            //Add it to the list
                             Styles.Add(singleItem);
                         }
                         break;
 
                     case "Voltage":
                         Voltages = new List<PricedItem>();
-                        //TODO document
                         foreach (var item in valueSetItems)
                         {
-                            PricedItem singleItem = new PricedItem
+                            var singleItem = new PricedItem
                             {
                                 Name = item.text,
                                 Price = item.price,
                                 Desc = item.text,
-                                Sku = item.sku
+                                Sku = item.sku,
+                                Img = "img_voltages.png"
                             };
                             //Add her to the list
                             Voltages.Add(singleItem);
@@ -125,10 +109,9 @@ namespace C3_Controls.Models.DataStructuring
 
                     case "Light Lens":
                         LightLens = new List<PricedItem>();
-                        //TODO document
                         foreach (var item in valueSetItems)
                         {
-                            PricedItem singleItem = new PricedItem
+                            var singleItem = new PricedItem
                             {
                                 Name = item.text,
                                 Price = item.price,
@@ -142,10 +125,9 @@ namespace C3_Controls.Models.DataStructuring
 
                     case "Sound Module":
                         SoundModule = new List<PricedItem>();
-                        //TODO document
                         foreach (var item in valueSetItems)
                         {
-                            PricedItem singleItem = new PricedItem
+                            var singleItem = new PricedItem
                             {
                                 Name = item.text,
                                 Price = item.price,
@@ -159,29 +141,45 @@ namespace C3_Controls.Models.DataStructuring
 
                     case "Position":
                         Positions = new List<PricedItem>();
-                        //TODO document
                         foreach (var item in valueSetItems)
                         {
-                            PricedItem singleItem = new PricedItem
+                            var singleItem = new PricedItem
                             {
                                 Name = item.text,
                                 Price = item.price,
                                 Desc = item.text,
                                 Sku = item.sku
                             };
-                            //Add her to the list
+
+                            //extra logic for assigning image path to position
+                            if (singleItem.Sku.EndsWith("A"))
+                                singleItem.Img = "img_light_amber.png";
+                            else if (singleItem.Sku.EndsWith("B"))
+                                singleItem.Img = "img_light_blue.png";
+                            else if (singleItem.Sku.EndsWith("G"))
+                                singleItem.Img = "img_light_green.png";
+                            else if (singleItem.Sku.EndsWith("R"))
+                                singleItem.Img = "img_light_red.png";
+                            else if (singleItem.Sku.EndsWith("W"))
+                                singleItem.Img = "img_light_white.png";
+                            //Add it to the list
                             Positions.Add(singleItem);
                         }
                         break;
-
                 }
-
-
             }
         }
 
-        
-       
+        public Dictionary<string, WTLItem[]> WtlMap { get; set; }
+        public string CmSetting => WebConfigurationManager.AppSettings["CurrentDatebase"];
 
+
+        public PricedItem OperatorType { get; set; }
+        public PricedItem ModuelDiameter { get; set; }
+        public List<PricedItem> Voltages { get; set; }
+        public List<PricedItem> Styles { get; set; }
+        public List<PricedItem> Positions { get; set; }
+        public List<PricedItem> SoundModule { get; set; }
+        public List<PricedItem> LightLens { get; set; }
     }
 }

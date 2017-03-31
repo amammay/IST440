ea47657620f1167812db7e969525e1f89fdf039e
Nico <nim5220@psu.edu>
2017-03-31 14:20:53 -0400

    assigned images somewhat to the datastructures

--- C3_Controls_Asp/C3 Controls/Models/DataStructuring/PTT_Data.cs
+++ C3_Controls_Asp/C3 Controls/Models/DataStructuring/PTT_Data.cs
@@ -97,6 +97,27 @@ namespace C3_Controls.Models.DataStructuring
                                 Desc = item.text,
                                 Sku = item.sku
                             };
+
+                            //extra logic for assigning image path to position
+                            if (singleItem.Sku.Equals("LA"))
+                                singleItem.Img = "img_lamp_amber.png";
+                            else if (singleItem.Sku.Equals("LB"))
+                                singleItem.Img = "img_lamp_blue.png";
+                            else if (singleItem.Sku.Equals("LG"))
+                                singleItem.Img = "img_lamp_green.png";
+                            else if (singleItem.Sku.Equals("LR"))
+                                singleItem.Img = "img_lamp_red.png";
+                            else if (singleItem.Sku.Equals("LW"))
+                                singleItem.Img = "img_lamp_white.png";
+                            else if (singleItem.Name.Equals("Clear Incandescent"))       //checked differently
+                                singleItem.Img = "img_lamp_clear_incandescent.png";
+                            else if (singleItem.Sku.Equals("F"))
+                                singleItem.Img = "img_lamp_clear_flashing_incandescent.png";
+                            else if (singleItem.Sku.Equals("NG"))
+                                singleItem.Img = "img_lamp_neon_green.png";
+                            else if (singleItem.Sku.Equals("NR"))
+                                singleItem.Img = "img_lamp_neon_red.png";
+
                             //Add her to the list
                             LampTypeColor.Add(singleItem);
                         }
@@ -113,6 +134,13 @@ namespace C3_Controls.Models.DataStructuring
                                 Desc = item.text,
                                 Sku = item.sku
                             };
+
+                            //assigning images to items
+                            //if (singleItem.Name.Contains("Aluminum"))
+                            //    singleItem.Img = "img_clamp_ring_aluminum.png";
+                            //else if (singleItem.Name.Contains("Polyester"))
+                            //    singleItem.Img = "img_clamp_ring_polyester.png";
+
                             //Add her to the list
                             ClampRing.Add(singleItem);
                         }
@@ -129,6 +157,17 @@ namespace C3_Controls.Models.DataStructuring
                                 Desc = item.text,
                                 Sku = item.sku
                             };
+
+                            //assigning image to each item
+                            //if (singleItem.Sku.Equals("IPBC"))
+                            //    singleItem.Img = "";
+                            //else if (singleItem.Sku.Equals("IPBCM"))
+                            //    singleItem.Img = "";
+                            //else if (singleItem.Sku.Equals("GIPBC"))
+                            //    singleItem.Img = "";
+                            //else if (singleItem.Sku.Equals("SIPBCM"))
+                            //    singleItem.Img = "";
+
                             //Add her to the list
                             LensType.Add(singleItem);
                         }
@@ -145,6 +184,21 @@ namespace C3_Controls.Models.DataStructuring
                                 Desc = item.text,
                                 Sku = item.sku
                             };
+
+                            //assigning image to each item
+                            if (singleItem.Sku.Equals("AR"))
+                                singleItem.Img = "img_lens_amber.png";
+                            else if (singleItem.Sku.Equals("BE"))
+                                singleItem.Img = "img_lens_blue.png";
+                            else if (singleItem.Sku.Equals("CR"))
+                                singleItem.Img = "img_lens_clear.png";
+                            else if (singleItem.Sku.Equals("GN"))
+                                singleItem.Img = "img_lens_green.png";
+                            else if (singleItem.Sku.Equals("RD"))
+                                singleItem.Img = "img_lens_red.png";
+                            else if (singleItem.Sku.Equals("WE"))
+                                singleItem.Img = "img_lens_white.png";
+
                             //Add her to the list
                             LensColor.Add(singleItem);
                         }
@@ -158,6 +212,7 @@ namespace C3_Controls.Models.DataStructuring
                                 Price = item.price,
                                 Desc = item.text,
                                 Sku = item.sku
+                                //, Img = "img_ip20_guards.png"
                             };
                         break;
                 }

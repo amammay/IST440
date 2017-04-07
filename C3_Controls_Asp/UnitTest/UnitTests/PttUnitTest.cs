b7083b14be6ff7a4395d2bb27a65326a373dd0c8
Alex Mammay <amm7100@psu.edu>
2017-04-07 13:01:09 -0400

    Added in unit test for both

---
+++ C3_Controls_Asp/UnitTest/UnitTests/PttUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using C3_Controls.Models.DataStructuring;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.UnitTests
+{
+    [TestClass]
+    public class PttUnitTest
+    {
+        private PTT_Data testdata { get; set; }
+
+        [TestMethod]
+        public void TestPttData()
+        {
+            testdata = new PTT_Data();
+
+            var testPttMap = testdata.PttMap;
+
+            Assert.IsNotNull(testPttMap);
+
+            Console.WriteLine("Staus of PTT Data test = Passed");
+
+        }
+    }
+}
--- C3_Controls_Asp/UnitTest/UnitTests/WtlUnitTest.cs
+++ C3_Controls_Asp/UnitTest/UnitTests/WtlUnitTest.cs
@@ -19,6 +19,8 @@ namespace UnitTest.UnitTests
 
             Assert.IsNotNull(returnMap);
 
+            Console.WriteLine(returnMap.Keys);
+
 
         }
     }
